f5b8b553ec9db0f5546e87c5a6599a647bca1844
PiotrekCieslakDev <piotr.cieslak299@gmail.com>
2025-10-24 00:08:48 +0200

    Category domain class - changed: added ownerId? so categories can be global or user's private

--- src/domain/Category.ts
+++ src/domain/Category.ts
@@ -4,17 +4,20 @@ export type CategoryProps = {
     id?: string;
     name: string;
     description?: string;
+    ownerId?: string;
 }
 
 export class Category {
     private readonly id: string;
     private name: string;
     private description: string;
+    private ownerId: string | null;
 
     constructor(props: CategoryProps) {
         this.id = props.id ?? uuidv4();
         this.name = props.name;
         this.description = props.description ?? "No description";
+        this.ownerId = props.ownerId ?? null;
     }
 
     public static create(name: string, description?: string): Category {
